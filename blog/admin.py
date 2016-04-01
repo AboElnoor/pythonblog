63c14619b04157c4b00bf8f8f167b367c5081ac3
Fatmasarhan <fatmasar7an@gmail.com>
2016-04-01 20:18:29 +0200

    Database changes and admin pannel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User,Article,Comment 
+admin.site.register(User)
+admin.site.register(Article)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,31 @@
 from __future__ import unicode_literals
-
 from django.db import models
+
 
 class User(models.Model):
 	user_name = models.CharField(max_length=200)
 	email = models.EmailField(max_length=200)
 	password = models.CharField(max_length=200)
+	user_image = models.ImageField(upload_to='userimages/')
 	role = models.IntegerField()
 	is_active = models.BooleanField(default=False)
+
+
+	def __str__(self):
+		return self.user_name
 
 class Article(models.Model):
 	title = models.CharField(max_length=200)
 	atricle_content = models.TextField()
-	image = models.CharField(max_length=255)
+	image = models.ImageField(upload_to='articleimages/')
 	publish_date = models.DateTimeField(null=True)
 	is_published = models.BooleanField(default=False)
 	user_id = models.ForeignKey(User,on_delete=models.CASCADE)
 	article_view = models.IntegerField()
 	article_mark = models.ManyToManyField(User, related_name="user_mark")
+
+	def __str__(self):
+		return self.title
 
 class Comment(models.Model):
 	comment_content = models.TextField()
@@ -29,3 +37,6 @@ class Comment(models.Model):
 class Tag(models.Model):
 	tag_name = models.CharField(max_length=200)
 	article_tag = models.ManyToManyField(Article)
+
+class BandWords(models.Model):
+	word = models.CharField(max_length=200)
