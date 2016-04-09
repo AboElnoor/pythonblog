3492de6da3bf94232b9e6977be91b95b9c0ffec2
AboElnouR <m.aboelnuor@gmail.com>
2016-04-09 10:21:33 +0200

    enhance models file making some changes to admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,37 @@
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
 from .models import *
 # Register your models here.
 
+
+class ArticleView(admin.ModelAdmin):
+    list_display = ('title', 'publish_date', 'is_published', 'article_view')
+    list_filter = ('publish_date', 'is_published')
+    fields = ('title', 'atricle_content', 'image', 'is_published', 'user_id')
+
+UserAdmin.list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
+UserAdmin.fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'age')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+UserAdmin.add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2')
+        }),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'age')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')})
+    )
+
 admin.site.register(Comment)
-admin.site.register(Article)
+admin.site.register(Article, ArticleView)
 admin.site.register(Tag)
-admin.site.register(UserProfile)
+admin.site.unregister(User)
+admin.site.register(UserProfile, UserAdmin)
 
 
 
--- blog/models.py
+++ blog/models.py
@@ -1,62 +1,65 @@
 from __future__ import unicode_literals
+from django.utils.translation import ugettext_lazy as _
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser, User
 
 
 class NewUser(models.Model):
-	user_name = models.CharField(max_length=200)
-	email = models.EmailField(max_length=200)
-	password = models.CharField(max_length=200)
-	user_image = models.ImageField()
-	role = models.IntegerField()
-	is_active = models.BooleanField(default=False)
+    user_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.CharField(max_length=200)
+    user_image = models.ImageField()
+    role = models.IntegerField()
+    is_active = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.user_name
 
-	def __str__(self):
-		return self.user_name
 
 class Article(models.Model):
-	title = models.CharField(max_length=200)
-	atricle_content = models.TextField()
-	image = models.ImageField()
-	publish_date = models.DateTimeField(null=True)
-	is_published = models.BooleanField(default=False)
-	user_id = models.ForeignKey(User,on_delete=models.CASCADE)
-	article_view = models.IntegerField(blank=True, null=True)
-	article_mark = models.ManyToManyField(User, related_name="user_mark", blank=True)
+    title = models.CharField(max_length=200)
+    atricle_content = models.TextField()
+    image = models.ImageField()
+    publish_date = models.DateTimeField(null=True)
+    is_published = models.BooleanField(default=False)
+    user_id = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    article_view = models.IntegerField(blank=True, null=True)
+    article_mark = models.ManyToManyField(User, related_name="user_mark", blank=True)
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
-	comment_content = models.TextField()
-	is_verified = models.BooleanField(default=False)
-	article_id = models.ForeignKey(Article,on_delete=models.CASCADE)
-	user_id = models.ForeignKey(User, related_name="user_comment",on_delete=models.CASCADE)
-	reply = models.ForeignKey('self', null=True, blank=True)
-	comment_like = models.ManyToManyField(User, blank=True)
+    comment_content = models.TextField()
+    is_verified = models.BooleanField(default=False)
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, related_name="user_comment", on_delete=models.CASCADE)
+    reply = models.ForeignKey('self', null=True, blank=True)
+    comment_like = models.ManyToManyField(User, blank=True)
 
-	def __str__(self):
-		return self.comment_content
+    def __str__(self):
+        return self.comment_content
+
 
 class Tag(models.Model):
-	tag_name = models.CharField(max_length=200)
-	article_tag = models.ManyToManyField(Article)
+    tag_name = models.CharField(max_length=200, unique=True,
+                                error_messages={
+                                    'unique': _("A user with that username already exists."),
+                                },
+                                )
+    article_tag = models.ManyToManyField(Article)
+
 
 class BandWords(models.Model):
-	word = models.CharField(max_length=200)
-	user_id=models.ForeignKey(User)
+    word = models.CharField(max_length=200)
+    user_id = models.ForeignKey(User)
 
-	def __str__(self):
-		return self.word
+    def __str__(self):
+        return self.word
 
 
-class UserProfile(models.Model):  
-    user = models.OneToOneField(User)
-    age= models.IntegerField()
+class UserProfile(User):
+    user = models.OneToOneField(User, related_name="add_info")
+    age = models.IntegerField(blank=True, null=True)
     user_image = models.ImageField(blank=True)
-
-  #   def __str__(self):
-		# return self.age
-
